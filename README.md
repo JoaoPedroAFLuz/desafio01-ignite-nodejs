5c80bf54af80dfce9c73f8e2e8bbf33010210c5e
João Pedro Luz <joao.pedro.luz@hotmail.com>
2022-11-27 10:43:16 -0300

    feat: get and create user

---
+++ README.md
@@ -0,0 +1,23 @@
+## 📃 *to dos* - Gerenciador de tarefas
+
+###  ✅ Requisitos
+
+- [x] Deve ser possível criar um novo usuário;
+- [x] Deve ser possível buscar os dados do usuário;
+- [ ] Deve ser possível criar um novo *to do*;
+- [ ] Deve ser possível listar todos os *to dos*;
+- [ ] Deve ser possível alterar o title e deadline de um *to do* existente;
+- [ ] Deve ser possível marcar um *to do* como concluído;
+- [ ] Deve ser possível excluir um *to do*;
+
+### 👩🏽‍💼 Regras de negócio
+
+- [x] Não deve ser possível cadastrar uma conta com um nome de usuário já existente;
+- [x] Não deve ser possível buscar dados de um usuário não existente;
+- [ ] Não deve ser possível criar um *to do* para um usuário não existente;
+- [ ] Não deve ser possível buscar os *to dos* de um usuário não existente;
+- [ ] Não deve ser possível alterar *to dos* de um usuário não existente;
+- [ ] Não deve ser possível alterar um *to do* não existente; 
+- [ ] Não deve ser possível marcar um *to do* não existente como concluído;
+- [ ] Não deve ser possível excluir um *to do* não existente;
+
--- src/index.js
+++ src/index.js
@@ -1,21 +1,54 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+
+  request.user = user;
+
+  next();
 }
 
+app.get('/users', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+
+  response.json(user);
+});
+
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const userAlreadyExists = users.some((user) => user.username === username);
+
+  if (userAlreadyExists) {
+    return response.status(400).json({ error: 'Username already exists' });
+  }
+
+  const newUser = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: [],
+  };
+
+  users.push(newUser);
+
+  response.status(201).json(newUser);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
